53da6ec84e271541c29f1e600f20193752c568fd
Kawamura Shingo <pannakoota@gmail.com>
2022-01-10 01:41:19 +0000

    initial commit

---
+++ lib/MyModule.pm
@@ -0,0 +1,32 @@
+package MyModule;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+MyModule -
+
+=head1 SYNOPSIS
+
+  use MyModule;
+
+=head1 DESCRIPTION
+
+MyModule is
+
+=head1 AUTHOR
+
+kawamurashingo E<lt>pannakoota@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
